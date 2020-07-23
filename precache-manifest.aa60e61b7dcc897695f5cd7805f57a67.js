d1d8c775ea2625dd92f5f1450a76933f3806057a
Eugene Ignatov <ignatovyevgen@gmail.com>
2020-07-23 18:48:31 -0500

    Updates

---
+++ precache-manifest.aa60e61b7dcc897695f5cd7805f57a67.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64484898bf3cbf8a032b32cdbcde79ca",
+    "url": "/street-car-cafe/index.html"
+  },
+  {
+    "revision": "88298dce25a066f0d982",
+    "url": "/street-car-cafe/static/css/main.89d6cc0f.chunk.css"
+  },
+  {
+    "revision": "9cbbca0400afafff7cce",
+    "url": "/street-car-cafe/static/js/2.e5d0c77d.chunk.js"
+  },
+  {
+    "revision": "2f0b178fdd378b725438d2210191759d",
+    "url": "/street-car-cafe/static/js/2.e5d0c77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88298dce25a066f0d982",
+    "url": "/street-car-cafe/static/js/main.7b5cae36.chunk.js"
+  },
+  {
+    "revision": "c8e0ac5677402fb497a6",
+    "url": "/street-car-cafe/static/js/runtime-main.b5d92fc9.js"
+  },
+  {
+    "revision": "492b672795f67dca3b59d1f23ff6b359",
+    "url": "/street-car-cafe/static/media/background-home.492b6727.jpg"
+  },
+  {
+    "revision": "0dee22296a51c0a0b32f2d0e1206263e",
+    "url": "/street-car-cafe/static/media/banner-slide-home.0dee2229.png"
+  },
+  {
+    "revision": "c7f51e2b19514be2852c3847f365d6d5",
+    "url": "/street-car-cafe/static/media/fooditem1.c7f51e2b.jpg"
+  },
+  {
+    "revision": "8794784411ecaf50f0a3c993296dbff8",
+    "url": "/street-car-cafe/static/media/fooditem2.87947844.jpg"
+  },
+  {
+    "revision": "e88326926fe03d9cf0549a15dd9be215",
+    "url": "/street-car-cafe/static/media/fooditem3.e8832692.jpg"
+  },
+  {
+    "revision": "3eabb358e32ccdaeeaf81a4aaf1f8749",
+    "url": "/street-car-cafe/static/media/fooditem4.3eabb358.jpg"
+  },
+  {
+    "revision": "58b439ace1e44f4aef28c25a495be632",
+    "url": "/street-car-cafe/static/media/fooditem5.58b439ac.jpg"
+  },
+  {
+    "revision": "f9639775ce75e5df820eae91049eea4e",
+    "url": "/street-car-cafe/static/media/fooditem6.f9639775.jpg"
+  },
+  {
+    "revision": "14937ec2b8549538fb5e461e9dbce886",
+    "url": "/street-car-cafe/static/media/street-restaurant-door002.14937ec2.jpg"
+  }
+]);
