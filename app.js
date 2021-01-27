ae999da66f0040d4bdaeb32a641eddd437476a44
Eugene Ignatov <ignatovyevgen@gmail.com>
2021-01-27 17:48:05 -0600

    connecting to heroku

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express();
-const port = 9000;
+const path = require('path');
+const port = process.env.PORT || 9000;
 
-const adminEditRouts = require('./routes/admin-edit');
-const menuItems = require('./routes/menu');
+// const adminEditRouts = require('./routes/admin-edit');
+// const menuItems = require('./routes/menu');
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use('/admin', adminEditRouts);
-app.use('/menu', menuItems);
+// app.use('/admin', adminEditRouts);
+// app.use('/menu', menuItems);
+// app.use('/', (req, res) => {
+//   res.send('hello');
+// });
 
-app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
-  res
-    .status(error.code || 500)
-    .json({ message: error.message || 'An unknown error occured!' });
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
+
+// app.use((error, req, res, next) => {
+//   if (res.headerSent) {
+//     return next(error);
+//   }
+//   res
+//     .status(error.code || 500)
+//     .json({ message: error.message || 'An unknown error occured!' });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
